ec264d60a23f1dd1f80d98e7f8bdb0c5ae882108
Swerwe <alexanderprivezentsev@mail.ru>
2024-02-12 12:33:25 +0000

    infinite parse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const Parser = require('./parser/Parser');
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 async function main() {
     const parser = new Parser();
-    await parser.parseAuctions();
-    await parser.parseStatistics();
+    // eslint-disable-next-line no-constant-condition
+    while (true){
+        await parser.parseAuctions();
+        await parser.parseStatistics();
+        console.log('Finished parsing, witing 1hr...');
+        await sleep(3600000);
+    }
+
 }
 main();

4d2e993a19239bce2364e1b99447e4b4d748240d
Swerwe <92936992+Swerwe@users.noreply.github.com>
2024-05-07 08:43:21 +0300

    Update parseAuctions.js

--- src/parser/methods/parseAuctions.js
+++ src/parser/methods/parseAuctions.js
@@ -5,7 +5,7 @@ const parseImages = require('../helpers/parseImages');
 const writeData = require('../../sql/writeData');
 const parseAuctions = async function (puppeteer) {
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'});
     const page =  await login(browser);
     //const data = JSON.parse(await fs.readFile('result.json', 'utf-8'));
     const data = await parseAucLinks(page);
